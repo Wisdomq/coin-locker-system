270bb52c229676204c78f666ae4485f373eaad33
Wisdom O. Odhiambo, MSc <50825417+Wisdomq@users.noreply.github.com>
2025-11-29 20:04:58 +0900

    Initialize mobile app with basic structure

---
+++ mobile-app/App.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View style={{ padding: 40 }}>
+      <Text>Coin Locker Mobile App</Text>
+    </View>
+  );
+}
